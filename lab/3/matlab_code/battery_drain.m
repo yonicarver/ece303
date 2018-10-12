8a850fc0360381af6affe331fb4c71ca9030cfbc
Farhan Muhammad <farhan@wego.com>
2018-10-12 11:24:51 -0400

    Update battery_drain.m

--- lab/3/matlab_code/battery_drain.m
+++ lab/3/matlab_code/battery_drain.m
@@ -5,7 +5,8 @@ close all;clear all;clc
 volts_per_division=2;
 time_range='3E-3';
 
-voltage_reading = [];
+voltage_reading = zeros(1, 7200);
+current_reading = zeros(1, 7200);
 
 % Create equipment connections and objects
 DMM=visa('agilent','USB0::0x2A8D::0xB318::MY58170025::0::INSTR');
@@ -31,54 +32,36 @@ fprintf(scope,'WAV:POINTS:MODE RAW');
 fprintf(scope,'WAV:POIN 2000');
 fprintf(scope,':WAVEFORM:BYTEORDER LSBFirst');
 
-
-
-tic
-timer_val = tic;
-
-
 i = 1;
 tic
-while (i && voltage_reading(length(voltage_reading)) > 4.8)
-	toc
-	pause(i-toc);
-	disp(voltage_reading(length(voltage_reading)));
-
-
-
+while (i)
+	pause(i-toc)
+ 
+        fprintf(scope,'DIG CHAN1;');
+	operationComplete = str2double(query(scope,'*OPC?'));
+	while ~operationComplete
+		operationComplete = str2double(query(scope,'*OPC?'));
+	end
+	preambleBlock = query(scope,':WAVEFORM:PREAMBLE?');
+	fprintf(scope,'WAV:DATA?;');
+	voltage_reading(i) = binblockread(scope,'uint16');		% appends voltage reading
+	disp(voltage_reading(i));
+        fread(scope,1);
+	current_reading(i) = str2double(query(DMM,'MEAS:CURR:DC?')); % Measure Current
 	i = i+1;
+        if (voltage_reading(i) < 4.8)
+            break;
+        end
 end
 
-			fprintf(scope,'DIG CHAN1;');
-			operationComplete = str2double(query(scope,'*OPC?'));
-			while ~operationComplete
-				operationComplete = str2double(query(scope,'*OPC?'));
-			end
-			preambleBlock = query(scope,':WAVEFORM:PREAMBLE?');
-			fprintf(scope,'WAV:DATA?;');
-			voltage_reading = [voltage_reading ; binblockread(scope,'uint16')];		% appends voltage reading
-
-			fread(scope,1);
-
-
-			current=str2double(query(DMM,'MEAS:CURR:DC?')); % Measure Current
-
-toc
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+while (1)
+    if (voltage_reading(end) == 0)
+        voltage_reading(end) = [];
+        current_reading(end) = [];
+    else
+        break;
+    end
+end
 
 % Close and delete instrument connections and objects
 fclose(DMM);
