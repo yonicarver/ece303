af3f68e0432ce78e13ddc6596d3d8c8ddea2fe95
Yoni Carver <yonicarver@gmail.com>
2018-11-12 17:01:47 -0500

    got GUI working, updated some arduino code

--- lab/6/arduino_code/ece303_lab6_DAQ/ece303_lab6_DAQ.ino
+++ lab/6/arduino_code/ece303_lab6_DAQ/ece303_lab6_DAQ.ino
@@ -16,15 +16,11 @@ DHT dht(DHT_PIN, DHT_TYPE);
 #define YAC_ESTOP_PIN      49
 
 // Thresholds
-#define COOLANT_THRESH_VAL 50
-#define TEMP_THRESH_VAL    30
-#define FAN_THRESH_VAL     100
+#define COOLANT_THRESH_VAL 50		// 50
+#define TEMP_THRESH_VAL    30		// 30
+#define FAN_THRESH_VAL     100	// 100
 
-//50
-//30
-//100
-
-// Matlab String
+// MATLAB string: Keys
 String str_estop_status = "estop_status: ";
 String str_main_relay = ", main_relay: ";
 String str_load_cell = ", load_cell: ";
@@ -35,7 +31,7 @@ String str_led_normal_op = ", led_normal_op: ";
 String str_led_high_temp = ", led_high_temp: ";
 String str_led_low_coolant = ", led_low_coolant: ";
 
-
+// MATLAB string initialization Values
 String estop_status = "test_estop";
 String main_relay_status = "test_relay";
 String temp_status = "test_temp";
@@ -44,22 +40,20 @@ String led_normal_op_status = "test_led_normalop";
 String led_high_temp_status = "test_led_hightemp";
 String led_low_coolant_status = "test_led_lowcoolant";
 
-
-
-// Declare load cell value and rpm as ints
-int load;
-int rpm;
+int load;		// load cell value
+int rpm;		// rpm value
 
 void setup() {
-  Serial.begin(9600);
-  Serial1.begin(9600);
-  dht.begin();
-  pinMode(BUZZER_PIN, OUTPUT);
-  pinMode(COOLANT_LED_PIN, OUTPUT);
-  pinMode(TEMP_LED_PIN, OUTPUT);
-  pinMode(NORMAL_LED_PIN, OUTPUT);
-  pinMode(FAN_PIN, OUTPUT);
-  pinMode(YAC_ESTOP_PIN, OUTPUT);
+  Serial.begin(9600);				// begin serial over USB
+  Serial1.begin(9600);				// begin serial1 over 18TX1 & 19RX1
+  dht.begin();						// initialize DHT library for temp/humidity sensor
+  
+  pinMode(BUZZER_PIN, 		OUTPUT);
+  pinMode(COOLANT_LED_PIN, 	OUTPUT);
+  pinMode(TEMP_LED_PIN, 		OUTPUT);
+  pinMode(NORMAL_LED_PIN, 	OUTPUT);
+  pinMode(FAN_PIN, 			OUTPUT);
+  pinMode(YAC_ESTOP_PIN, 	OUTPUT);
 
 }
 
@@ -106,6 +100,7 @@ void loop() {
     led_high_temp_status = "on";
     led_low_coolant_status = "on";
   }
+  
   else if (waterLevel <= COOLANT_THRESH_VAL) {
     tone(BUZZER_PIN, 500);
     digitalWrite(NORMAL_LED_PIN, LOW);
@@ -122,6 +117,7 @@ void loop() {
     led_high_temp_status = "off";
     led_low_coolant_status = "on";
   }
+  
   else if (temp >= TEMP_THRESH_VAL) {
     tone(BUZZER_PIN, 500);
     digitalWrite(NORMAL_LED_PIN, LOW);
@@ -138,11 +134,12 @@ void loop() {
     led_high_temp_status = "on";
     led_low_coolant_status = "off";
   }
+  
   else {
+    noTone(BUZZER_PIN);
     digitalWrite(NORMAL_LED_PIN, HIGH);
     digitalWrite(TEMP_LED_PIN, LOW);
     digitalWrite(COOLANT_LED_PIN, LOW);
-    noTone(BUZZER_PIN);
 
     digitalWrite(YAC_ESTOP_PIN, HIGH);
 
@@ -161,19 +158,21 @@ void loop() {
 
 // Collect and send data to MATLAB
 void make_string(){
-  String packet_to_matlab =
-    str_estop_status + estop_status +
-    str_main_relay + main_relay_status +
-    str_load_cell + load +
-    str_optical_counter + rpm +
-    str_temp + temp_status +
-    str_coolant + coolant_status +
-    str_led_normal_op + led_normal_op_status +
-    str_led_high_temp + led_high_temp_status +
-    str_led_low_coolant + led_low_coolant_status
-    ;
+	
+	// Assemble string to be sent to MATLAB
+	String packet_to_matlab =
+	    str_estop_status + estop_status +
+	    str_main_relay + main_relay_status +
+	    str_load_cell + load +
+	    str_optical_counter + rpm +
+	    str_temp + temp_status +
+	    str_coolant + coolant_status +
+	    str_led_normal_op + led_normal_op_status +
+	    str_led_high_temp + led_high_temp_status +
+	    str_led_low_coolant + led_low_coolant_status
+	    ;
 
-    char packet_to_mlab[10] = "000111000";
+//    char packet_to_mlab[10] = "000111000";		// testing
 
 //  if (Serial.available())
     Serial.println(packet_to_matlab);
